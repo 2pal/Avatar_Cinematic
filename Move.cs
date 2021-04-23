34d5ac7b65a81451bd106fafd4be7fc5527c5c9a
2pal <omertopal46@gmail.com>
2021-04-23 22:02:46 +0300

    Cycle path

---
+++ Move.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    private Transform ThisTransform = null;
+    public float speed = 1f;
+    public AnimationCurve curve;
+    void Awake()
+    {
+        ThisTransform = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ThisTransform.position += speed * ThisTransform.forward * Time.deltaTime;
+        float y = curve.Evaluate(Mathf.PingPong(Time.time,a 1f));
+        ThisTransform.position = new Vector3(ThisTransform.position.x, y, ThisTransform.position.z);
+
+    }
+}
