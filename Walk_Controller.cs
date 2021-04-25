b956e37fc6738a260e5c8fb1bb7626ddacd867ee
2pal <omertopal46@gmail.com>
2021-04-25 17:59:14 +0300

    Cutscene

---
+++ Walk_Controller.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Walk_Controller : MonoBehaviour
+{
+
+    private Animator Animator1 = null;
+    void Start()
+    {
+        Animator1 = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float Vert = Input.GetAxis("Vertical");
+        Animator1.SetFloat("Walk", Vert);
+    }
+}
